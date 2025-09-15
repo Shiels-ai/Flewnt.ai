42b3347ce451815a5db40628de6b0d7517ecb598
Sam Shiels <SamShiels@proton.me>
2025-09-15 21:21:50 +0200

    Renaming

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/flewnt.ai/", // <-- replace with your repo name if deploying to GitHub Pages
+  base: "/Flewnt.ai/", // <-- replace with your repo name if deploying to GitHub Pages
 });
